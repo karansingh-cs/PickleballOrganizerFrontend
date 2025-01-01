31e926dc1939a0ab6eb627f40e1b0685713d788d
karansingh-cs <karanbir97@outlook.com>
2025-01-01 12:45:16 -0600

    Added the Tournament Page and Home Page

---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Pickleball Tournament Organizer!</h1>
+      <p>Manage your tournaments, players, and schedules effortlessly.</p>
+      <button onClick={() => alert('Get Started!')}>Get Started</button>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Tournaments.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Tournaments() {
+  const [tournaments, setTournaments] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/tournaments")
+      .then(response => setTournaments(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Pickleball Tournaments</h1>
+      <ul>
+        {tournaments.map(tournament => (
+          <li key={tournament.id}>
+            {tournament.name} - {tournament.location} on {tournament.date}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Tournaments;
