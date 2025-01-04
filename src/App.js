7e3e8059d7faf52c6c66f42569e755b968c9d24f
karansingh-cs <karanbir97@outlook.com>
2025-01-04 00:19:51 -0600

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Tournaments from "./Pages/Tournaments";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
