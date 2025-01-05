34883e8b0184741b7195daf64c9a9e485d2289c1
karansingh-cs <karanbir97@outlook.com>
2025-01-04 22:27:15 -0600

    resolved the navbar error

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tournaments" element={<Tournaments />} />
